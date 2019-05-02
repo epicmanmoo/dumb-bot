b303a54948a9996de7da1be63263f7dcca6c0f1c
epicmanmoo <mooaz09@gmail.com>
2019-05-01 21:50:01 -0700

    Edits!!!

--- botTesting/ListOfCommands.cs
+++ botTesting/ListOfCommands.cs
@@ -6,6 +6,7 @@ using System.Net;
 using Newtonsoft.Json.Linq;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace botTesting
 {
@@ -146,7 +147,7 @@ namespace botTesting
                     Embed.AddField("Example: ", result.list.ElementAt(nextRand).example);
                     Embed.AddField("Likes: ", result.list.ElementAt(nextRand).thumbs_up);
                     Embed.AddField("Dislikes: ", result.list.ElementAt(nextRand).thumbs_down);
-                    Embed.AddField("Written on: ", fixedDate);                
+                    Embed.AddField("Written on: ", fixedDate);
                     await Context.Channel.SendMessageAsync("", false, Embed.Build());
                     await Context.Channel.SendMessageAsync("**Imprecise translation:** " + result.list.ElementAt(nextRand).definition);
                     return;
@@ -164,9 +165,125 @@ namespace botTesting
             }
         }
         [Command("translate")]
-        public async Task Translate()
+        public async Task Translate(string language1, string language2, [Remainder] string msg)
         {
-            
+            //EMBED FOR THIS!!
+            Dictionary<string, string> langs = new Dictionary<string, string>();
+            langs["Azerbaijan"] = "az";
+            langs["Malayalam"] = "ml";
+            langs["Albanian"] = "sq";
+            langs["Maltese"] = "mt";
+            langs["Amharic"] = "am";
+            langs["Macedonian"] = "mk";
+            langs["English"] = "en";
+            langs["Maori"] = "mi";
+            langs["Arabic"] = "ar";
+            langs["Marathi"] = "mr";
+            langs["Armenian"] = "hy";
+            langs["Mari"] = "mhr";
+            langs["Afrikaans"] = "af";
+            langs["Mongolian"] = "mn";
+            langs["Basque"] = "eu";
+            langs["German"] = "de";
+            langs["Bashkir"] = "ba";
+            langs["Nepali"] = "ne";
+            langs["Belarusian"] = "be";
+            langs["Norwegian"] = "no";
+            langs["Bengali"] = "bn";
+            langs["Punjabi"] = "pa";
+            langs["Burmese"] = "my";
+            langs["Papiamento"] = "pap";
+            langs["Bulgarian"] = "bg";
+            langs["Persian"] = "fa";
+            langs["Bosnian"] = "bs";
+            langs["Polish"] = "pl";
+            langs["Welsh"] = "cy";
+            langs["Portuguese"] = "pt";
+            langs["Hungarian"] = "hu";
+            langs["Romanian"] = "ro";
+            langs["Vietnamese"] = "vi";
+            langs["Russian"] = "ru";
+            langs["Haitian(Creole)"] = "ht";
+            langs["Cebuano"] = "ceb";
+            langs["Galician"] = "gl";
+            langs["Serbian"] = "sr";
+            langs["Dutch"] = "nl";
+            langs["Sinhala"] = "si";
+            langs["Hill Mari"] = "mrj";
+            langs["Slovakian"] = "sk";
+            langs["Greek"] = "el";
+            langs["Slovenian"] = "sl";
+            langs["Georgian"] = "ka";
+            langs["Swahili"] = "sw";
+            langs["Gujarati"] = "gu";
+            langs["Sundanese"] = "su";
+            langs["Danish"] = "da";
+            langs["Tajik"] = "tg";
+            langs["Hebrew"] = "he";
+            langs["Thai"] = "th";
+            langs["Yiddish"] = "yi";
+            langs["Tagalog"] = "tl";
+            langs["Indonesian"] = "id";
+            langs["Tamil"] = "ta";
+            langs["Irish"] = "ga";
+            langs["Tatar"] = "tt";
+            langs["Italian"] = "it";
+            langs["Telugu"] = "te";
+            langs["Icelandic"] = "is";
+            langs["Turkish"] = "tr";
+            langs["Spanish"] = "es";
+            langs["Udmurt"] = "udm";
+            langs["Kazakh"] = "kk";
+            langs["Uzbek"] = "uz";
+            langs["Kannada"] = "kn";
+            langs["Ukrainian"] = "uk";
+            langs["Catalan"] = "ca";
+            langs["Urdu"] = "ur";
+            langs["Kyrgyz"] = "ky";
+            langs["Finnish"] = "fi";
+            langs["Chinese"] = "zh";
+            langs["French"] = "fr";
+            langs["Korean"] = "ko";
+            langs["Hindi"] = "hi";
+            langs["Xhosa"] = "xh";
+            langs["Croatian"] = "hr";
+            langs["Khmer"] = "km";
+            langs["Czech"] = "cs";
+            langs["Laotian"] = "lo";
+            langs["Swedish"] = "sv";
+            langs["Latin"] = "la";
+            langs["Scottish"] = "gd";
+            langs["Latvian"] = "lv";
+            langs["Estonian"] = "et";
+            langs["Lithuanian"] = "lt";
+            langs["Esperanto"] = "eo";
+            langs["Luxembourgish"] = "lb";
+            langs["Javanese"] = "jv";
+            langs["Malagasy"] = "mg";
+            langs["Japanese"] = "ja";
+            langs["Malay"] = "ms";
+            string langFromAndTo = language1 + "-" + language2;
+            string key = "trnsl.1.1.20190501T085131Z.60ba4708835f8d1f.10422b658ea3bfcf81a3c05d9da973a83920a8ae";
+            var encoded = Uri.EscapeUriString(msg);
+            WebClient client = new WebClient();
+            string value = client.DownloadString("https://translate.yandex.net/api/v1.5/tr.json/translate?lang=" + langFromAndTo + "&key=" + key + "&text=" + encoded);
+            var result = JsonConvert.DeserializeObject<YandexTranslate.RootObject>(value);
+            var translatedText = result.text.ElementAt(0);
+            foreach (KeyValuePair<string, string> item in langs)
+            {
+                EmbedBuilder listOfLanguages = new EmbedBuilder();
+                listOfLanguages.AddField("Key", item.Key);
+                if (item.Value.Contains(language1))
+                {
+                    language1 = item.Key;
+                }
+                if (item.Value.Contains(language2))
+                {
+                    language2 = item.Key;
+                    break;
+                }
+            }
+            await Context.Channel.SendMessageAsync("`" + msg + "`" + " translated from `" + language1 + "` is `" + translatedText + "` in `" + language2 + "`");
         }
     }
 }
---
+++ botTesting/YandexTranslate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace botTesting
+{
+    [Serializable]
+    class YandexTranslate
+    {
+        [Serializable]
+        public class RootObject
+        {
+            public int code { get; set; }
+            public string lang { get; set; }
+            public List<string> text { get; set; }
+        }
+    }
+}
