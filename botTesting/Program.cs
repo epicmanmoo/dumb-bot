ab1515f00d62e5cf822c013d7e1b25fa64c544d6
Mooaz Nasir <mooaz09@gmail.com>
2020-06-17 12:56:54 -0700

    location

--- botTesting/Program.cs
+++ botTesting/Program.cs
@@ -69,7 +69,7 @@ namespace botTesting
         {
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArguments("headless");
-            driver = new ChromeDriver("C:\\Users\\mooaz\\Desktop\\Github\\DiscordBot\\botTesting\\", chromeOptions);
+            driver = new ChromeDriver("chromedriver.exe location", chromeOptions);
             driver.Manage().Window.Maximize();
         }
         private async Task Disconnected(Exception arg)
